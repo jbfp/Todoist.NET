8e5fa6cc1f5ca0dfdc6701ece22e66522c34240a
Jakob Pedersen <jakobp10@gmail.com>
2011-10-08 18:42:10 +0200

    Did it again, forgot TimeZoneOffset.cs

---
+++ Todoist.NET/TimeZoneOffset.cs
@@ -0,0 +1,50 @@
+﻿namespace Todoist.NET
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TimeZoneOffset
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string GmtString { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int HourOffset { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsDaylightSavingsTime { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MinuteOffset { get; set; }
+
+        internal TimeZoneOffset()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gmtValue"></param>
+        /// <param name="hourOffset"></param>
+        /// <param name="minuteOffset"></param>
+        /// <param name="isDaylightSavingsTime"></param>
+        public TimeZoneOffset(string gmtValue,
+                              int hourOffset,
+                              int minuteOffset,
+                              bool isDaylightSavingsTime)
+        {
+            GmtString = gmtValue;
+            HourOffset = hourOffset;
+            MinuteOffset = minuteOffset;
+            IsDaylightSavingsTime = isDaylightSavingsTime;
+        }
+    }
+}
